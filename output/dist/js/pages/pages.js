eab0f5c715b249e160ee7f7cc0ab3f910b9a65dd
zorluozan <61868614+zorluozan@users.noreply.github.com>
2022-07-04 16:43:23 +0300

    some content changes

--- output/dist/js/pages/pages.js
+++ output/dist/js/pages/pages.js
@@ -1,6 +1,9 @@
 var pageList = [
   "about.html",
-  "blog-detail.html",
+  "blog-detail-1.html",
+  "blog-detail-2.html",
+  "blog-detail-3.html",
+  "blog-detail-4.html",
   "blog.html",
   "contact.html",
   "contract-menu.html",
